8fc2a5a1adfdec14ca56725dbc65946335a7937e
xan678 <alpha98689@gmail.com>
2021-12-08 01:01:07 -0500

    Day 2

--- Vectors/mountain.cpp
+++ Vectors/mountain.cpp
@@ -1,9 +1,51 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
+int highest_mountain(vector<int> arr)
+{
+    int n = arr.size();
+    int largest = 0;
+    int width = -1;
 
-int main(){
-    
+    for (int i = 1; i <= n - 2; i++)
+    {
+        // Check if a[i] is peak or not;
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+        {
+            // do some work
+            int cnt = 1;
+            // cnt backwards (left)
+            int j = i;
+            while (j >= 1 && arr[j] > arr[j - 1])
+            {
+                cnt++;
+                j--;
+            }
+            // cnt forward (right)
+            while (i <= n - 2 && arr[i] > arr[i + 1])
+            {
+                cnt++;
+                i++;
+            }
+
+            if (cnt > width)
+            {
+                width = cnt;
+            }
+        }
+        // else
+        // {
+        //     i++;
+        // }
+    }
+
+    return width;
+}
+
+int main()
+{
+    vector<int> arr{5, 6, 1, 2, 3, 4, 5, 4, 3, 2, 0, 1, 2, 3, -2, 4};
+    cout << highest_mountain(arr) << endl;
     return 0;
 }
