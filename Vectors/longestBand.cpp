b605da518fad7206bf9ff51f1058fa8938607e38
xan678 <alpha98689@gmail.com>
2021-12-08 03:35:34 -0500

    Day 2

--- Vectors/longestBand.cpp
+++ Vectors/longestBand.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 using namespace std;
+
+int longestChain(vector<int> arr)
+{
+    int n = arr.size();
+
+    unordered_set<int> s;
+    for (int x : arr)
+    {
+        s.insert(x);
+    }
+
+    int largestLen = INT_MIN;
+
+    for (auto element : s)
+    {
+        int parent = element - 1;
+        if (s.find(parent) == s.end())
+        {
+            // find entire band/ chain starting from element
+            int next_no = element + 1;
+            int cnt = 1;
+
+            while (s.find(next_no) != s.end())
+            {
+                s.erase(next_no);
+                next_no++;
+                cnt++;
+            }
+
+            if (cnt > largestLen)
+            {
+                largestLen = cnt;
+            }
+        }
+    }
+
+    return largestLen;
+}
 
 int main()
 {
-
+    vector<int> arr{1, 9, 3, 0, 18, 2, 4, 10, 7, 12, 6};
+    cout << longestChain(arr);
     return 0;
 }
