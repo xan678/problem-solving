426d62db9ef1583aa88a9d5e4781120d81a64a8a
xan678 <alpha98689@gmail.com>
2021-12-08 23:48:11 -0500

    Day 3

--- Vectors/subarraySort.cpp
+++ Vectors/subarraySort.cpp
@@ -2,8 +2,60 @@
 #include <vector>
 using namespace std;
 
+bool outOfOrder(vector<int> arr, int idx)
+{
+    int x = arr[idx];
+    if (idx == 0)
+    {
+        return x > arr[1];
+    }
+    if (idx == arr.size() - 1)
+    {
+        return x < arr[idx - 1];
+    }
+
+    return x > arr[idx + 1] || x < arr[idx - 1];
+}
+
+pair<int, int> subArraySort(vector<int> arr)
+{
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int x = arr[i];
+        if (outOfOrder(arr, i))
+        {
+            smallest = min(smallest, x);
+            largest = max(largest, x);
+        }
+    }
+    cout << smallest << " " << largest << endl;
+    // Now find the right index where smallest and the largest should be present so that the entire array get's sorted
+    if (smallest == INT_MAX)
+    {
+        return {-1, -1};
+    }
+
+    int left = 0;
+    while (smallest >= arr[left])
+    {
+        left++;
+    }
+    int right = arr.size() - 1;
+    while (largest <= arr[right])
+    {
+        right--;
+    }
+
+    return {left, right};
+}
+
 int main()
 {
-
+    vector<int> arr = {1, 2, 3, 4, 5, 8, 6, 7, 9, 10, 11};
+    auto p = subArraySort(arr);
+    cout << p.first << " and " << p.second << endl;
     return 0;
 }
